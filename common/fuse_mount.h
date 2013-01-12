d219087ff026f8c3f06c7e9df4e5ac01000286cb
Benjamin Fleischer <fleiben@gmail.com>
2013-01-12 05:11:12 +0100

    (kext, support) Move OSXFUSE_FSTYPENAME_PREFIX to fuse_version.h

--- common/fuse_mount.h
+++ common/fuse_mount.h
@@ -43,12 +43,8 @@ enum {
     FUSE_FSSUBTYPE_INVALID = -1,
 };
 
-/* File system type name. */
-
-#define FUSE_FSTYPENAME_PREFIX OSXFUSE_FS_TYPE "_"
-
 /* Courtesy of the Finder, this is 1 less than what you think it should be. */
-#define FUSE_FSTYPENAME_MAXLEN (MFSTYPENAMELEN - sizeof(FUSE_FSTYPENAME_PREFIX) - 1)
+#define FUSE_FSTYPENAME_MAXLEN (MFSTYPENAMELEN - sizeof(OSXFUSE_FSTYPENAME_PREFIX) - 1)
 
 /* mount-time flags */
 #define FUSE_MOPT_IGNORE                 0x0000000000000000ULL
