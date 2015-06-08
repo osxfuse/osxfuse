19132916185ebb40e5e762905072c09f736a8be9
Benjamin Fleischer <fleiben@gmail.com>
2015-06-09 00:57:35 +0200

    (build) Add preliminary support for OS X 10.11

--- build.d/targets/release.sh
+++ build.d/targets/release.sh
@@ -79,7 +79,7 @@ function release_build
     # Build distribution package
 
     build_target_invoke distribution build -s 10.5 -d 10.5 -c Release \
-                                           --kext=10.5 --kext=10.6 --kext="10.7->10.6" --kext="10.8->10.6" --kext=10.9 --kext="10.10->10.9" \
+                                           --kext=10.5 --kext=10.6 --kext="10.7->10.6" --kext="10.8->10.6" --kext=10.9 --kext="10.10->10.9" --kext="10.11->10.9" \
                                            --macfuse \
                                            --code-sign-identity="${BUILD_TARGET_OPTION_CODE_SIGN_IDENTITY}" \
                                            --product-sign-identity="${BUILD_TARGET_OPTION_PRODUCT_SIGN_IDENTITY}"
