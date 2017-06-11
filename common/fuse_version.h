fe5d0d4efa2a1e228e5fbe8af99898619135cbc5
Benjamin Fleischer <fleiben@gmail.com>
2017-06-11 22:24:38 +0200

    Bump version to 3.6.0

--- common/fuse_version.h
+++ common/fuse_version.h
@@ -28,7 +28,7 @@
 
 /* Version */
 
-#define OSXFUSE_VERSION_LITERAL             3.5.8
+#define OSXFUSE_VERSION_LITERAL             3.6.0
 #define OSXFUSE_TIMESTAMP                   __DATE__ ", " __TIME__
 
 #define OSXFUSE_VERSION                     FUSE_PP_STRINGIFY(OSXFUSE_VERSION_LITERAL)
