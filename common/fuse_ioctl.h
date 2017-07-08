6863b73d87288ffff7e73fcf7d3d13bc50c5b15e
Benjamin Fleischer <fleiben@gmail.com>
2017-07-09 01:06:14 +0200

    Remove FUSEDEVIOCGETHANDSHAKECOMPLETE ioctl

--- common/fuse_ioctl.h
+++ common/fuse_ioctl.h
@@ -12,9 +12,6 @@
 // Get mounter's pid
 #define FUSEDEVGETMOUNTERPID           _IOR('F', 1,  u_int32_t)
 
-// Check if FUSE_INIT kernel-user handshake is complete
-#define FUSEDEVIOCGETHANDSHAKECOMPLETE _IOR('F', 2,  u_int32_t)
-
 // Mark the daemon as dead
 #define FUSEDEVIOCSETDAEMONDEAD        _IOW('F', 3,  u_int32_t)
 
